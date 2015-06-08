a3441a667cb4040a76a021946f7f0f6fcd0c32fe
Aliza Levinger <alizalevinger@gmail.com>
2015-06-08 17:00:15 -0400

    added image resources in different resolutions

--- ImageViewer/app/src/main/java/com/aliza/imageviewer/MainActivity.java
+++ ImageViewer/app/src/main/java/com/aliza/imageviewer/MainActivity.java
@@ -1,17 +1,26 @@
 package com.aliza.imageviewer;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends android.app.Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager=getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            setContentView(R.layout.activity_main);
+
+
     }
 
 
